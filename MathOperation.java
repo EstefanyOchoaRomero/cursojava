eec1c0e6448bfa9a27b318bad02a5204d7833649
EstefanyOchoaRomero <segatorrico@gmail.com>
2024-09-23 21:06:16 +0200

    ejercicio 11

---
+++ MathOperation.java
@@ -0,0 +1,31 @@
+
+
+public class MathOperation {
+
+
+    public static void main(String[] args) {
+
+        
+        double raiz = Math.sqrt(36);
+
+        System.out.println(raiz);
+
+        double base = 5, exponente = 2;
+        double resultado  = Math.pow(base, exponente);
+
+        System.out.println(resultado);
+
+        int numero = -8;
+
+        System.out.println(Math.abs(numero));
+        
+
+
+
+        
+
+
+
+
+    }
+}
