fedebfe7498957e8a8eda58a737901698268771f
EstefanyOchoaRomero <segatorrico@gmail.com>
2024-09-24 16:35:32 +0200

    ejercicio 19

--- Voto.java
+++ Voto.java
@@ -3,14 +3,14 @@ import javax.swing.JOptionPane;
 
 public class Voto{
     public static void main(String[] arg) {
-        String a = JOptionPane.showInputDialog("ingrese la esdad");
+        String a = JOptionPane.showInputDialog("Ingrese la edad");
         int edad = Integer.parseInt(a);
 
         if (edad>=18) {
-            JOptionPane.showMessageDialog(null, "la persona es mayor de edad");
+            JOptionPane.showMessageDialog(null, "Puede votar");
         
         } else {
-            JOptionPane.showMessageDialog(null, "la persona es menor de edad");
+            JOptionPane.showMessageDialog(null, "NO puede votar");
 
         }
             
